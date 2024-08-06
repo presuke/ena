7b46fcc79a6a9020fa34a630c5d02dab500a7437
renoneve <reno.neve@gmail.com>
2024-08-06 23:59:08 +0900

    5th commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+    <link rel="stylesheet" href="/css/app.css">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
