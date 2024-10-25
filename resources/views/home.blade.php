a5dc986ec6ea6d40dae0f2becb9f5c74b55a0496
renoneve <reno.neve@gmail.com>
2024-10-25 23:59:33 +0900

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,18 +25,24 @@
                                     <div style="float:left;">
                                         <img src="/image/icon_solor.png" class="iconModule">
                                     </div>
-                                    <div class="box" style="float: left; margin-top:20px; width: calc(50vw-50px); height: 50px;">
+                                    <div class="box" style="float: left; margin-top:20px; width: 50%; height: 50px;">
                                         <span class="box__line"></span>
                                         <span class="box__line"></span>
                                     </div>
                                     <div style="clear:left; float:left;">
                                         <img src="/image/icon_grid.png" class="iconModule">
                                     </div>
-                                    <div class="box" style="float: left; margin-top:20px; width: calc(50vw-75px); height: 50px;">
+                                    <div class="box" style="float: left; margin-top:20px; width: 50%; height: 50px;">
                                         <span class="box__line"></span>
                                     </div>
                                     <div style="float:left;">
                                         <img src="/image/icon_hi.png" class="iconModule">
+                                    </div>
+                                    <div class="box" style="float: left; margin-top:20px; width: 50%; height: 50px;">
+                                        <span class="box__line"></span>
+                                    </div>
+                                    <div style="float:left;">
+                                        <img src="/image/use.png" class="iconModule">
                                     </div>
                                     <div>@{{Math.floor( (new Date() - new Date(hybridInverter.create_at)) / 1000 / 60)}}分前データ</div>
                                     <ul class="">
