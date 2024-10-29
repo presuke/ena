8126acf9ad55bd4b043a8499b199a9e3a9a099e9
renoneve <reno.neve@gmail.com>
2024-10-30 02:24:25 +0900

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,9 +83,9 @@
                                                 </div>
                                                 <div style="width:min(15vw, 150px); float:left;">
                                                     <img src="/image/icon_battery.png" class="iconModule">
+                                                    <div class="fontMain" style="margin-top:-3vw;">⚡️@{{hybridInverter.battery_voltage.toLocaleString()}}v</div>
                                                 </div>
                                                 <div style="width:min(15vw, 150px); float:left;">
-                                                    <div class="fontMain" style="float:left;">⚡️@{{hybridInverter.battery_voltage.toLocaleString()}}v</div>
                                                     <v-progress-circular :model-value="hybridInverter.battery_soc" :color="[hybridInverter.battery_soc<30?'red':hybridInverter.battery_soc<50?'orange':hybridInverter.battery_soc<70?'green':'yellow']" style="clear:left; width:5vw; height:5vw; max-width:50px; max-height:50px;">
                                                         <div>@{{ Math.ceil(hybridInverter.battery_soc)}}%</div>
                                                     </v-progress-circular>
