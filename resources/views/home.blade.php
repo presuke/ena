04f22c0768443ac3b2aaa2754e486e127481aff1
renoneve <reno.neve@gmail.com>
2024-11-01 23:41:01 +0900

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -197,13 +197,24 @@
                         <div>
                             <h4>現在の設定</h4>
                             <div style="margin-bottom: 10px; border-radius: 5px; padding:5px; background-color: black; border:solid thin gray;">
-                                <div style="float:left;">
-                                    <img src="/image/icon_solor.png" class="iconModule">
-                                    <div class="fontMain">1st</div>
+                                <div style="float:left; width:80px; height:80px; text-align:center;">
+                                    <img src="/image/icon_solor.png" class="iconModule" style="float:left; width:100%; height:100%;">
+                                    <div class="fontMain" style="font-size:100%; color:white;">Main</div>
                                 </div>
-                                <div style="float:left;">
-                                    <img src="/image/icon_grid.png" class="iconModule">
-                                    <div class="fontMain">2nd</div>
+                                <div style="float:left; width:60px; height:60px;" text-align:center;">
+                                    <img src="/image/icon_grid.png" class="iconModule" style="float:left; width:100%; height:100%;">
+                                    <div class="fontMain" style="font-size:100%; color:white;">Sub</div>
+                                </div>
+                                <div style="clear:left;"></div>
+                            </div>
+                            <div style="margin-bottom: 10px; border-radius: 5px; padding:5px; background-color: black; border:solid thin gray;">
+                                <div style="float:left; width:80px; height:80px; text-align:center;">
+                                    <img src="/image/icon_grid.png" class="iconModule" style="float:left; width:100%; height:100%;">
+                                    <div class="fontMain" style="font-size:100%; color:white;">Main</div>
+                                </div>
+                                <div style="float:left; width:60px; height:60px;" text-align:center;">
+                                    <img src="/image/icon_solor.png" class="iconModule" style="float:left; width:100%; height:100%;">
+                                    <div class="fontMain" style="font-size:100%; color:white;">Sub</div>
                                 </div>
                                 <div style="clear:left;"></div>
                             </div>
