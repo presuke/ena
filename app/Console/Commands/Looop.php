4cfdc12e8970f57634a5687d98465dddf7df6929
renoneve <reno.neve@gmail.com>
2024-11-01 11:19:24 +0900

    modified:   app/Console/Commands/Looop.php 	modified:   app/Console/Commands/Looop.php

--- app/Console/Commands/Looop.php
+++ app/Console/Commands/Looop.php
@@ -30,8 +30,6 @@ class Looop extends Command
      */
     public function handle()
     {
-        echo "loop";
-        /*
         try {
             //noごとの最新を取ってくる
             $sql = "SELECT * FROM hidata AS h1 WHERE user='" . $token->name . "' AND create_at = (SELECT MAX(create_at) FROM hidata AS h2 WHERE h1.no = h2.no)";
@@ -52,6 +50,5 @@ class Looop extends Command
             $ret['errors'][] = $ex;
             return Command::FAILURE;
         }
-            */
     }
 }
