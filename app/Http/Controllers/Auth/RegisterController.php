e6a008077b35344b8ce88211e1772595ba334eab
renoneve <reno.neve@gmail.com>
2024-11-09 15:30:11 +0900

    modified:   app/Http/Controllers/Auth/RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/authed';
 
     /**
      * Create a new controller instance.
