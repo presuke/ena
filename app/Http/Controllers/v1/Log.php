6af4647a32b6bda79477e67b161d0b1dbd332b2b
renoneve <reno.neve@gmail.com>
2024-12-07 08:25:30 +0900

    modified:   app/Http/Controllers/v1/Log.php

--- app/Http/Controllers/v1/Log.php
+++ app/Http/Controllers/v1/Log.php
@@ -98,7 +98,7 @@ class Log extends BaseController
             $sql = "SELECT * FROM hidata AS h1 WHERE user='" . $token->name . "' AND create_at = (SELECT MAX(create_at) FROM hidata AS h2 WHERE h1.no = h2.no)";
             $data = DB::select($sql);
             $sql = "SELECT password FROM users WHERE email='" . $token->name . "'";
-            $decrypted = $sql;
+            $decrypted = DB::select($sql);
             $ret['code'] = 0;
             $ret['data'] = $data;
             $ret['pass'] = $decrypted;
